03ede4f385b94858093a0a1e425cc9b2488c77e1
Chen Jianjun <chenjj.yz@gmail.com>
2025-11-19 14:08:03 +0800

    Add github/spec-kit support

--- scripts/build-uos.sh
+++ scripts/build-uos.sh
@@ -14,7 +14,7 @@ fi
 docker build -t ccsafeenv-linux-builder -f Dockerfile.uos .
 
 # 运行如下命令启动容器
-# docker run -it -v ~/.claude/settings.json:/home/app/.claude/settings.json -v ~/.claude.json:/home/app/.claude.json -v $(pwd):/app1 -v $(pwd)/.root_claude:/home/app/.claude ccsafeenv-linux-builder /bin/bash
+# mkdir -p .root_claude && docker run -it -v ~/.claude/settings.json:/home/app/.claude/settings.json -v ~/.claude.json:/home/app/.claude.json -v $(pwd):/app1 -v $(pwd)/.root_claude:/home/app/.claude  ccsafeenv-linux-builder /bin/bash
 
 # 运行如下命令启动claude
 # claude --dangerously-skip-permissions
